8a97e87b6ee9863a413783bec216951d55259e08
Aynekulu <Aynekulu4341@gmail.com>
2025-10-06 09:20:50 +0300

    config: Update Django settings and app configuration

--- django_web/spam_app/apps.py
+++ django_web/spam_app/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class SpamAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'spam_app'
--- django_web/spam_project/settings.py
+++ django_web/spam_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'spam_app',  # This line must be present
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ML_SERVICE_URL = os.getenv('ML_SERVICE_URL', 'http://localhost:8001')
